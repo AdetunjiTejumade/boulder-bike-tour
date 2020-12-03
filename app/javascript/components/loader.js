a3a3fa376e021f6093ac2ae4b2b06c4db0825663
AdetunjiTejumade <tejumadeadetunji@gmail.com>
2020-12-04 03:16:47 +0900

    add a preloder for photos

---
+++ app/javascript/components/loader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../assets/stylesheets/loader.css'
+
+function Loader() {
+    return (
+        <div className="loader">
+            <div>
+            </div>
+            <div>
+            </div>
+            <div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader;
