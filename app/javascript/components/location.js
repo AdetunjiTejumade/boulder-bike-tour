dcc38dac5b68a8efbf50ac8c1601b6b502ff2dc7
AdetunjiTejumade <tejumadeadetunji@gmail.com>
2020-12-06 23:25:46 +0900

    add locations page

--- app/javascript/components/location.js
+++ app/javascript/components/location.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+import { riders } from './riderInfo'
 
 function Location() {
+    const token = 'pk.eyJ1IjoidGVqdW1hZGUiLCJhIjoiY2tpY256OTR0MWI2ZTJ5bzVydHk0aDJmcCJ9._zaIJ1d9pVt2WX9YDOR5iw'
+    const position = [40.0150, -105.2705]
+
     return (
+
         <>
+            <MapContainer center={position} zoom={13} className="h-full mx-6">
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+
+                {
+                    riders.map((items, index) => {
+                        const { firstName, lastName, coordinates } = items;
+                        const [lat, lon] = coordinates;
+                        return (
+                            <Marker position={{lat, lon}} key={index}>
+                                <Popup>
+                                    {firstName } {lastName}
+                            </Popup>
+                            </Marker>
+                        )
+                    })
+                }
+
+            </MapContainer>
         </>
+
     )
+
 }
 export default Location;
---
+++ app/javascript/components/riderInfo.js
@@ -0,0 +1,145 @@
+export const riders = [
+    {
+        "firstName": "Dustin",
+        "lastName": "Green",
+        "city": "Hickory Hills",
+        "state": "IL",
+        "coordinates": [40.00, -105.35]
+
+    },
+    {
+        "firstName": "Jason",
+        "lastName": "Finn",
+        "city": "Huntington Beach",
+        "state": "CA",
+        "coordinates": [39.95, -105.24]
+
+    },
+    {
+        "firstName": "Howard",
+        "lastName": "Thompson",
+        "city": "Hale",
+        "state": "MO",
+        "coordinates": [40.06,-105.26]
+    },
+    {
+        "firstName": "Maggie",
+        "lastName": "Lantz",
+        "city": "Dublin",
+        "state": "OH",
+        "coordinates": [40.03,-105.23]
+    },
+    {
+        "firstName": "Lawrence",
+        "lastName": "Duran",
+        "city": "Duran",
+        "state": "FL",
+        "coordinates":[40.04,-105.23]
+    },
+    {
+        "firstName": "Irene",
+        "lastName": "Molina",
+        "city": "Tucson",
+        "state": "AZ",
+        "coordinates":[39.96,-105.22]
+    },
+    {
+        "firstName": "Nancy",
+        "lastName": "Garner",
+        "city": "Baltimore",
+        "state": "MD",
+        "coordinates":[39.98,-105.21]
+    },
+    {
+        "firstName": "Tara",
+        "lastName": "Taylor",
+        "city": "Minneapolis",
+        "state": "MN",
+        "coordinates":[40.00,-105.25]
+    },
+    {
+        "firstName": "Alejandro",
+        "lastName": "Smith",
+        "city": "Scarborough",
+        "state": "ME",
+        "coordinates":[40.02,-105.26]
+    },
+    {
+        "firstName": "Tricia",
+        "lastName": "Renshaw",
+        "city": "Mystic",
+        "state": "CT",
+        "coordinates":[40.02, -105.30]
+    },
+    {
+        "firstName": "Travis",
+        "lastName": "Cook",
+        "city": "Twin Lakes",
+        "state": "WI",
+        "coordinates":[40.01,-105.20]
+    },
+    {
+        "firstName": "Joan",
+        "lastName": "Brooks",
+        "city": "Norfolk",
+        "state": "VA",
+        "coordinates":[39.98,-105.24]
+    },
+
+    {
+        "firstName": "Joseph",
+        "lastName": "Rodgers",
+        "city": "Wayne",
+        "state": "NJ",
+        "coordinates":[39.99,-105.25]
+    },
+    {
+        "firstName": "Matthew",
+        "lastName": "Gregson",
+        "city": "Brashear",
+        "state": "MO",
+        "coordinates":[40.01,-105.29]
+    },
+    {
+        "firstName": "Katie",
+        "lastName": "Dunlap",
+        "city": "Marion",
+        "state": "VA",
+        "coordinates":[39.97,-105.27]
+    },
+    {
+        "firstName": "Leo",
+        "lastName": "Howard",
+        "city": "Dallas",
+        "state": "TX",
+        "coordinates":[39.97,-105.33]
+    },
+    {
+        "firstName": "Maria",
+        "lastName": "Baisden",
+        "city": "Raleigh",
+        "state": "NC",
+        "coordinates":[40.00,-105.29]
+    },
+    {
+        "firstName": "Sandy",
+        "lastName": "Townsend",
+        "city": "Newport ",
+        "state": "PA",
+        "coordinates":[40.05,-105.30]
+    },
+    {
+        "firstName": "Melinda",
+        "lastName": "Stephenson",
+        "city": "Kahului",
+        "state": "HI",
+        "coordinates":[39.99,-105.31]
+    },
+    {
+        "firstName": "Jason",
+        "lastName": "Nichols",
+        "city": "Alexandria",
+        "state": "VA",
+        "coordinates":[40.01,105.32]
+    },
+]
