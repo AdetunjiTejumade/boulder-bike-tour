ea2b815eecf0a58c650cf64b0c1606887e8fa601
AdetunjiTejumade <tejumadeadetunji@gmail.com>
2020-12-13 03:48:00 +0900

    update

--- app/javascript/components/riders.js
+++ app/javascript/components/riders.js
@@ -7,6 +7,7 @@ function Riders() {
     return (
         <>
             <div className="relative  min-h-full">
+                <h1 className="text-4xl text-center roboto">Riders</h1>
                 <div className="pb-32 pt-12">
                     <div className="sm:grid grid-cols-3 mx-4 lg:grid-cols-4">
                         {riders.map((items, index) => {
